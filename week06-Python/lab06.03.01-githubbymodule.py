7a7c6f5631def014d84539d1a73e5b8200d6ebec
Declan Reidy <G00364690@gmit.ie>
2020-01-04 01:34:26 +0100

    Change keys

--- week06-Python/lab06.03.01-githubbymodule.py
+++ week06-Python/lab06.03.01-githubbymodule.py
@@ -2,7 +2,7 @@ from github import Github
 import requests
 
 # remove the minus sign from the key
-g = Github("32ac307d2ce1061fd8beecb4324002df97b57533")
+g = Github("32ac307d2ce1061fd8beecb4324002df97b5753-3")
 
 repo = g.get_repo("G00364690/Datarepresentation")
 print(repo)
