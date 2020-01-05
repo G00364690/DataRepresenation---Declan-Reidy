6f0d40843aa73b098b8b1505fd1aaaac8a6fc1f3
Declan Reidy <G00364690@gmit.ie>
2020-01-05 19:25:59 +0100

    web interface

--- Big Project/createDatabase.py
+++ Big Project/createDatabase.py
@@ -11,4 +11,8 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 sql = "CREATE TABLE test3 (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)"
+# This does not update the sql localhost as expected. 
+#The code cannot be run again with same table name as the database/table are created
+# But they do not appear under show tables.
+
 cursor.execute(sql)
